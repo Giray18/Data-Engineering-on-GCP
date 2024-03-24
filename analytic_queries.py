e73715623dd3ab53d5652f30fb3c714ae6e6b519
Giray18 <cihangiray.oner@gmail.com>
2024-03-24 18:02:32 +0000

    goal_chart_window_created

--- analytic_queries.py
+++ analytic_queries.py
@@ -53,3 +53,60 @@ with DAG(dag_id= 'analytic_queries_as_table',
     write_disposition="WRITE_TRUNCATE",
     use_legacy_sql=False,
 )
+
+
+    total_offsides_chart_all_teams = BigQueryExecuteQueryOperator(
+    task_id="offside_chart",
+    sql="""
+    WITH total_offsides_chart AS (
+    SELECT REPLACE(team2_name,'&','and') AS team_names,SUM(team2_stat.offsides) AS total_offsides FROM `capable-memory-417812.premiership.epl_2022_2023_07_02_2024` GROUP BY team_names 
+    UNION ALL
+    SELECT REPLACE(team1_name,'&','and') AS team_names,SUM(team1_stat.offsides) AS total_offsides FROM `capable-memory-417812.premiership.epl_2022_2023_07_02_2024` GROUP BY team_names)
+    SELECT team_names,SUM(total_offsides) AS total_offsides_caught FROM total_offsides_chart GROUP BY team_names ORDER BY total_offsides_caught DESC
+    """,
+    destination_dataset_table=f"capable-memory-417812.premiership.total_offsides_chart_all_teams",
+    write_disposition="WRITE_TRUNCATE",
+    use_legacy_sql=False,
+)
+
+    total_goal_chart_teams  = BigQueryExecuteQueryOperator(
+    task_id="goal_chart_weekly",
+    sql="""
+    WITH
+    RECURSIVE game_scores AS(
+    SELECT
+    SPLIT(REGEXP_EXTRACT_ALL(event[
+      OFFSET
+        (0)],r'\,(.*?)\.')[0],",") AS game_scores_goals,
+    CAST(RIGHT(matchweek,2)AS INT64) AS game_week
+    FROM
+    `capable-memory-417812.premiership.epl_2022_2023_07_02_2024`),
+    game_scores_by_teams AS(
+    SELECT
+    REGEXP_REPLACE(game_scores_teams,r'\s[0-9]+','') AS team_name,
+    game_week,
+    CAST(RIGHT(game_scores_teams,1) AS INT64) AS goals
+    FROM
+    game_scores,
+    UNNEST(game_scores_goals) AS game_scores_teams)
+    SELECT
+    team_name,
+    game_week,
+    goals as goals_this_week,
+    LAG(goals) OVER (PARTITION BY team_name ORDER BY game_week) AS goals_last_week,
+    goals - LAG(goals) OVER (PARTITION BY team_name ORDER BY game_week) AS diff_last_week,
+    SUM(goals) OVER(PARTITION BY team_name ORDER BY game_week) AS running_total_goal,
+    SUM(goals) OVER(PARTITION BY team_name) AS total_goals,
+    ROUND((SUM(goals) OVER(PARTITION BY team_name ORDER BY game_week)/SUM(goals) OVER(PARTITION BY team_name)),2) AS perc_to_total_goals,
+    FROM
+    game_scores_by_teams
+    ORDER BY
+    team_name ,game_week 
+    """,
+    destination_dataset_table=f"capable-memory-417812.premiership.total_goal_chart_weekly",
+    write_disposition="WRITE_TRUNCATE",
+    use_legacy_sql=False,
+)
+
+
+
