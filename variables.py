bb50bd1e75646c406a7b7d5d24a30a8f1306b9ac
C. Giray <cihangiray.oner@gmail.com>
2024-04-02 16:41:11 +0200

    Delete variables.py

--- variables.py
+++
@@ -1,9 +0,0 @@
-def variables_dict():
-    ''' Returns a dict consisting of variable names to pass into task functions'''
-    dict_variables = {'bucket_name' : 'tryoutdavar', 'project_id' : 'capable-memory-417812',
-    'converted_bucket_name' : 'tryoutdavar_jsonl_conv' }
-    return dict_variables
-
-
-if __name__ == '__main__':
-    variables_dict()
